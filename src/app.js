1067fb2fdb7a2c4f8f7d687d7301c0794b5eb524
Iordache, Mihai <mihai.iordache@misys.com>
2019-09-09 13:38:08 +0300

    module1

--- src/app.js
+++ src/app.js
@@ -10,6 +10,6 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('', (req, res) => res.render('index', {title:'Index'}));
+app.get('/', (req, res) => res.render('index', {title:'Index'}));
 
 app.listen(3000, () => console.log('PS Project runs on port 3000'))
